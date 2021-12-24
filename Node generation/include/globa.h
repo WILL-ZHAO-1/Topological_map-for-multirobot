69059c1cdb2cb3b4ffa83e10e8f0036f25bfbe4d
Will-Zhao <85217573+WILL-ZHAO-1@users.noreply.github.com>
2021-12-24 11:07:32 +0800

    Update globa.h

--- Node generation/include/globa.h
+++ Node generation/include/globa.h
@@ -1,5 +1,5 @@
 #pragma once
-//原始图像路径
+//Original image path
 #define IMAGE "map/ee.png"
 #define IMAGE3 "Creat/first_node.png"
 #define IMAGE4 "Creat/final.png"
@@ -8,26 +8,29 @@
 #define IMAGE7 "Creat/pmap.png"
 #define IMAGE8 "Creat/edg.png"
 
-//#define TEXXT "first_node.txt" //第一次生成的节点
-#define TEXT1 "Creat/sim_node.txt" //简化后的节点列表
-#define TEXT2 "Creat/ang_node.txt" //识别出来的角点列表
-#define TEXT3 "Creat/tran_node.txt" //地图的原点是左下角，图像处理的原点是左上角，坐标转换后的坐标
+//#define TEXXT "first_node.txt" //Node generated for the first time
+#define TEXT1 "Creat/sim_node.txt" //Simplified node list
+#define TEXT2 "Creat/ang_node.txt" //List of recognized corners
+#define TEXT3 "Creat/tran_node.txt" 
+//The origin of the map is the lower left corner, 
+//the origin of image processing is the upper left corner,
+//the coordinates after coordinate conversion
 
 #define TEXT4 "Creat/match_node.txt"
 
-//节点生成参数
-//模板大小
+//Node generation parameters
+//Template size
 #define ker_size 13
-//节点简化距离(一个机器人宽度8个单位（0.05m/像素）)
+//Node simplification distance (a robot width 8 units (0.05m/pixel))
 #define sim_dis 13
-//跳跃生成节点距离
+//Jump spawning node distance
 #define Leap_dis 13
 
-//节点匹配参数
-#define rows_dis 15 //行距
-#define cols_dis 15 //列距
+//Node matching parameters
+#define rows_dis 15 
+#define cols_dis 15 
 
-////拓扑地图
+//Topological map
 //#define infinite 999
 
 
