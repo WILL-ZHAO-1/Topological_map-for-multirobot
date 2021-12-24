b5b60b892f36c806fd568ef07fe3c6fdacd8587b
Will-Zhao <85217573+WILL-ZHAO-1@users.noreply.github.com>
2021-12-24 10:45:26 +0800

    Update Node_main.cpp

--- Node generation/src/Node_main.cpp
+++ Node generation/src/Node_main.cpp
@@ -8,21 +8,21 @@ using namespace std;
 
 int main()
 {
+	//Generate node
 	node_generate t1(IMAGE);
-	//角点检测
-	//t1.ang_point();
-	//节点简化
+	//Node simplification
 	node_simplify t2(t1.generate_node());
 	t1.result();
 
-	//节点匹配
-	//node_matching m;
-	//m.Matches();
+	//Node matching
+	node_matching m;
+	m.Matches();
 
-	//创建拓扑地图
-	//Graph map, map2;
-	////map.short_path(0,23);
-	//map2.Heuristically_path(43,9);
+	//Create a topology map
+	Graph map, map2;
+	//Calculate the global path
+	map.short_path(0,23);
+	map2.Heuristically_path(43,9);
 	system("pause");
 	return 0;
 }
